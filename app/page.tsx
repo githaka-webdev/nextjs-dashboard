38cfd1339beeca716e81673a10d97a2929f92452
githaka-webdev <githaka.webdev@gmail.com>
2024-10-15 11:48:15 +0300

    feat(fonts): updated some fonts

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
+import { lusitana } from '@/app/ui/fonts';
 
 export default function Page() {
     return (
@@ -12,7 +13,11 @@ export default function Page() {
             <div className='mt-4 flex grow flex-col gap-4 md:flex-row'>
                 <div className='flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20'>
                     <p
-                        className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}
+                        className={[
+                            `text-xl text-gray-800 md:text-3xl md:leading-normal`,
+                            lusitana.className,
+                            'antialiased',
+                        ].join(' ')}
                     >
                         <strong>Welcome to Acme.</strong> This is the example
                         for the{' '}
@@ -24,7 +29,7 @@ export default function Page() {
                         </a>
                         , brought to you by Vercel.
                     </p>
-                    <div className={styles.shape} />
+                    <div className={[styles.shape].join(' ')} />
                     <Link
                         href='/login'
                         className='flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base'
